7e993d97547ec7a84ad1d267e5dfbd6165daf476
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-22 21:27:29 -0400

    Add validity handler
    - POST: verify message

---
+++ validate.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type Chirpie struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respError(w, http.StatusInternalServerError, err, "Error: Could not decode parameters")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respError(w, http.StatusBadRequest, err, "Error: Chirpie is too long")
+		return
+	}
+
+	respBody := Chirpie{
+		Valid: true,
+	}
+	respJSON(w, 200, respBody)
+}
+
+func respError(w http.ResponseWriter, code int, err error, msg string) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	type errResp struct {
+		Error string `json:"error"`
+	}
+	errBody := errResp{
+		Error: msg,
+	}
+	respJSON(w, code, errBody)
+}
+
+func respJSON(w http.ResponseWriter, code int, jsonResp interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	data, err := json.Marshal(jsonResp)
+	if err != nil {
+		log.Printf("Error marshalling JSON %s\n", err)
+	}
+	w.Write(data)
+}
